9306e32f2bd5af7337394ef7594dbf080635d232
atomicwest <ogjdne@gmail.com>
2017-02-18 15:42:43 -0800

    add tempConvert.cpp
    source file

---
+++ tempConvert.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void main ()
+{
+
+	double ftemp;
+	double ctemp;
+
+  cout << "Input temp" << endl;
+  cin >> ctemp;
+  ftemp = (9/5)*ctemp + 32;
+  cout << "your ftemp is" << ftemp << endl;
+  cout << "" << endl;
+  system("PAUSE");
+
+};
